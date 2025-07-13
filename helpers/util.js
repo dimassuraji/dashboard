b33453210dd3e8eb9cfdf5cc481b619ab33bff22
dim <emailkamu@example.com>
2025-07-13 16:03:53 +0700

    feat: update MQTT client and dashboard view

--- helpers/util.js
+++ helpers/util.js
@@ -1,6 +1,16 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+//konfigurasi koneksi database lokal
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
+//konfigurasi koneksi database railway
 // const pool = new Pool({
 //   user: process.env.DB_USER,
 //   host: process.env.DB_HOST,
@@ -10,17 +20,18 @@ const { Pool } = require("pg");
 //   ssl: { rejectUnauthorized: false },
 // });
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: process.env.CA_CERT.replace(/\\n/g, "\n"),
-  },
-});
+//konfigurasi koneksi database aiven
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+//   ssl: {
+//     rejectUnauthorized: true,
+//     ca: process.env.CA_CERT.replace(/\\n/g, "\n"),
+//   },
+// });
 
 class Response {
   constructor(data, success = true) {
